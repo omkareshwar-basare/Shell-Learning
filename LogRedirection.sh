09f65eeb7846a313e23c1a7e04ac4d466f061ae5
Basareom <ombasare@gmail.com>
2026-01-31 22:59:04 +0530

    log redirection

--- LogRedirection.sh
+++ LogRedirection.sh
@@ -5,7 +5,7 @@ LOG_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
-   echo "Please run the script with root user access"
+   echo "Please run the script with root user access" | tee -a $LOGS_FILE
    exit 1
 fi
 
@@ -13,7 +13,7 @@ mkdir -p $LOG_FOLDER
 
 VALIDATE(){
 if [ $1 -ne 0 ]; then
-   echo "$2...FAILURE"
+   echo "$2...FAILURE" | tee -a $LOGS_FILE
    exit 1
 else
     echo "$2...SUCCESS"
@@ -22,7 +22,7 @@ fi
 
 
 dnf install nginx -y &>> $LOGS_FILE
-VALIDATE $? "Installing Nginx"
+VALIDATE $? "Installing Nginx" 
 
 dnf install mysql -y &>> $LOGS_FILE
 VALIDATE $? "Installing MySQL"
