9a20bfb581c3e9fadc67a2f36d25a89a638492b3
Basareom <ombasare@gmail.com>
2026-01-31 23:14:22 +0530

    Installation Loop

---
+++ Installationloop.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOG_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+   echo "Please run the script with root user access" | tee -a $LOGS_FILE
+   exit 1
+fi
+
+mkdir -p $LOG_FOLDER
+
+VALIDATE(){
+if [ $1 -ne 0 ]; then
+   echo "$2...FAILURE" | tee -a $LOGS_FILE
+   exit 1
+else
+    echo "$2...SUCCESS" | tee -a $LOGS_FILE
+fi
+}
+
+
+for package in $@   #sudo sh Insatallationloop.sh nginx mysql nodejs
+do
+   dnf install $package -y &>> $LOGS_FILE
+   VALIDATE $? "$package Installing" 
+done
+
+
