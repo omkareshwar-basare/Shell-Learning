7d0b2328e3f0e8654878680d30a8d01cdf9587db
Basareom <ombasare@gmail.com>
2026-01-31 18:44:30 +0530

    Read Input From User

---
+++ ReadInputFromUser.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Enter your name___________:"
+read name
+echo "Hello My Dear Friend $name"
+echo "Enter password__________"
+read -s password
+echo "Let us see password $password"
